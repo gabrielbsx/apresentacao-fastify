4206b731cd800aff65fc4b6d60378bff5b2d6a34
Gabriel <gabrielturing@gmail.com>
2023-05-09 13:31:28 -0300

    feat: server with some hook example

---
+++ src/server-with-some-hook.ts
@@ -0,0 +1,32 @@
+import fastify, {
+  type FastifyReply,
+  type FastifyRequest,
+  type HookHandlerDoneFunction
+} from 'fastify'
+import { exit } from 'node:process'
+
+const someHook = (_request: FastifyRequest, _reply: FastifyReply, done: HookHandlerDoneFunction): void => {
+  console.log('Executando o hook antes do handler')
+  done()
+}
+
+const server = fastify()
+
+server.addHook('onRequest', someHook)
+
+const handlerListening = (error: Error | null, address: string): void => {
+  if (error != null) {
+    server.log.error(error)
+    exit(1)
+  }
+  server.log.info(`Server listening on ${address}`)
+}
+
+server.get('/', async (_request, reply) => {
+  return await reply.send('hook example')
+})
+
+server.listen({
+  port: 3000,
+  host: 'localhost'
+}, handlerListening)
