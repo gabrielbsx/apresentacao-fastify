405d98cbbea2c6181f2ae9834c9aaa997814da6d
Gabriel <gabrielturing@gmail.com>
2023-05-09 13:30:21 -0300

    refactor: rename application var to semantic name

--- src/server-simple.ts
+++ src/server-simple.ts
@@ -1,21 +1,21 @@
 import { exit } from 'node:process'
 import fastify from 'fastify'
 
-const application = fastify({ logger: true })
+const server = fastify({ logger: true })
 
 const handlerListening = (error: Error | null, address: string): void => {
   if (error != null) {
-    application.log.error(error)
+    server.log.error(error)
     exit(1)
   }
-  application.log.info(`Server listening on ${address}`)
+  server.log.info(`Server listening on ${address}`)
 }
 
-application.get('/', async (_request, reply) => {
+server.get('/', async (_request, reply) => {
   return await reply.send({ hello: 'world' })
 })
 
-application.listen({
+server.listen({
   port: 3000,
   host: 'localhost'
 }, handlerListening)
