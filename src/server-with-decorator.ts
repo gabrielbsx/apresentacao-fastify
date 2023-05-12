0f0d310db09e41d098c81f271fd7f0e40b3e72c5
Gabriel <gabrielturing@gmail.com>
2023-05-12 02:07:01 -0300

    feat: ensure that server implements some decorator

---
+++ src/server-with-decorator.ts
@@ -0,0 +1,28 @@
+import fastify from 'fastify'
+
+interface SomeDecoratorParams {
+  message: string
+}
+
+async function main (): Promise<void> {
+  const server = fastify({
+    logger: true
+  })
+
+  server.decorate('someDecorator', (someDecoratorParams: SomeDecoratorParams): void => {
+    console.log('sua mensagem é:', someDecoratorParams.message)
+  })
+
+  server.get('/', async (_request, reply) => {
+    server.someDecorator({ message: 'Hello World' })
+    return await reply.send('decorator example')
+  })
+}
+
+void main()
+
+declare module 'fastify' {
+  interface FastifyInstance {
+    someDecorator: (someDecoratorParams: SomeDecoratorParams) => void
+  }
+}
