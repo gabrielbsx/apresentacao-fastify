9139af9918002aaa140795d8ea4b21bc3738512e
Gabriel <gabrielturing@gmail.com>
2023-05-13 17:48:49 -0300

    feat: add native benchmark

--- benchmark/autocannon.ts
+++ benchmark/autocannon.ts
@@ -2,6 +2,7 @@ import autocannon, { type Result } from 'autocannon'
 
 const fastifyUrl = 'http://localhost:3000'
 const expressUrl = 'http://localhost:3001'
+const nativeUrl = 'http://localhost:3002'
 
 async function runBenchmark (url: string): Promise<Result> {
   const result = await autocannon({
@@ -13,33 +14,41 @@ async function runBenchmark (url: string): Promise<Result> {
   return result
 }
 
-async function runBenchmarkComparison (): Promise<void> {
+async function runBenchmarkComparison (trynumber: number = 1): Promise<void> {
   console.log('Rodando benchmark para o Fastify')
   const fastifyResult = await runBenchmark(fastifyUrl)
 
   console.log('Rodando benchmark para o Express')
   const expressResult = await runBenchmark(expressUrl)
 
-  console.log('Fastify vs Express')
-  compareResults(fastifyResult, expressResult)
+  console.log('Rodando benchmark para o Node')
+  const nativeResult = await runBenchmark(nativeUrl)
+
+  compareResults(fastifyResult, expressResult, nativeResult, trynumber)
 }
 
-function compareResults (fastifyResult: Result, expressResult: Result): void {
+function compareResults (fastifyResult: Result, expressResult: Result, nativeResult: Result, trynumber: number): void {
+  console.log('\nFastify vs Express vs Node - Resultados da comparação:', trynumber)
+
   const fastifyRequestsPerSecond = fastifyResult.requests.average
   const expressRequestsPerSecond = expressResult.requests.average
-  const difference = fastifyRequestsPerSecond - expressRequestsPerSecond
+  const nativeRequestsPerSecond = nativeResult.requests.average
 
-  console.log(`Fastify requisições por segundo: ${fastifyRequestsPerSecond}`)
+  console.log(`\nFastify requisições por segundo: ${fastifyRequestsPerSecond}`)
   console.log(`Express requisições por segundo: ${expressRequestsPerSecond}`)
-  console.log(`A diferença é de ${difference.toFixed(2)} requisições por segundo`)
+  console.log(`Node requisições por segundo: ${nativeRequestsPerSecond}\n`)
 
-  if (fastifyRequestsPerSecond > expressRequestsPerSecond) {
-    console.log('Fastify é mais rápido')
-  } else if (fastifyRequestsPerSecond < expressRequestsPerSecond) {
-    console.log('Express é mais rápido')
-  } else {
-    console.log('Fastify e Express são igualmentes rápidos')
-  }
+  const ranking = [
+    { name: 'Fastify', requestsPerSecond: fastifyRequestsPerSecond },
+    { name: 'Express', requestsPerSecond: expressRequestsPerSecond },
+    { name: 'Node', requestsPerSecond: nativeRequestsPerSecond }
+  ]
+
+  ranking.sort((a, b) => b.requestsPerSecond - a.requestsPerSecond)
+
+  ranking.forEach((item, index) => {
+    console.log(`${index + 1}º lugar: ${item.name} com ${item.requestsPerSecond} requisições por segundo`)
+  })
 }
 
 runBenchmarkComparison().catch(console.error)
